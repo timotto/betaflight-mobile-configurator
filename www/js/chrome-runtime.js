b42665f3314df49d210bded7eda1dfa7823e436c
Anders G <anders.gallon@animechtechnologies.com>
2017-05-06 19:04:35 +0200

    Add proxy for chrome.runtime to provide the extension-manifest (containing version-info).

---
+++ www/js/chrome-runtime.js
@@ -0,0 +1,19 @@
+(function(ctx) {
+	var Runtime = function() {
+		this.manifest = {};
+
+		var self = this;
+
+		$.getJSON('./manifest.json', function (data) {
+			self.manifest = data;
+		});
+	};
+
+	Runtime.prototype.getManifest = function() {
+		return this.manifest;
+	};
+
+	ctx.chrome = ctx.chrome || {};
+	ctx.chrome.runtime = new Runtime();
+})(window);
+
