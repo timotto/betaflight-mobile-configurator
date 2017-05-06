bcb9df8ebd042d99813e1a69b9c318aef5ccc67b
Anders G <anders.gallon@animechtechnologies.com>
2017-05-06 19:04:00 +0200

    Add proxy for chrome.i18n to provide localization support.

---
+++ www/js/chrome-i18n.js
@@ -0,0 +1,28 @@
+(function(ctx) {
+	var I18n = function() {
+		this.languages = ['en'];
+		this.language = 'en';
+		this.locales = {};
+
+		var self = this;
+
+		this.languages.forEach(function(language) {
+			$.getJSON('./_locales/' + language + '/messages.json', function (data) {
+				self.locales[language] = data;
+			});
+		});
+	};
+
+	I18n.prototype.getMessage = function(id, params) {
+		var phrase = this.locales[this.language][id];
+		var message = phrase ? phrase.message : id;
+
+		return message.replace(/\$\d/g, function(attr) {
+			var num = Number(attr[1]) - 1;
+			return params[num] || '';
+		});
+	};
+
+	ctx.chrome = ctx.chrome || {};
+	ctx.chrome.i18n = new I18n();
+})(window);
