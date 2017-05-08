cb3ac027e1987919ba169e3f3be9a1f0a145ab2d
Anders G <anders.gallon@animechtechnologies.com>
2017-05-08 20:18:48 +0200

    Implement local-storage functionality for chrome.storage.local, to make expert-mode status and other properties persistent.

--- www/js/chrome-storage.js
+++ www/js/chrome-storage.js
@@ -1,13 +1,45 @@
-// TODO: Add functionality for storing / retrieving preferences?
 (function(ctx) {
 	var Storage = function() {
 		this.local = new Local();
 	};
 
-	var Local = function() {};
+	var Local = function() {
+		this.isValid = typeof(Storage) !== "undefined";
+	};
 
-	Local.prototype.get = function() {
+	Local.prototype.get = function(arg1,  callback) {
+		if (!this.isValid || !callback) return;
 
+		var result = {};
+		var propNames = [];
+
+		if (arg1 instanceof String || typeof arg1 === 'string') {
+			propNames.push(arg1);
+		} else if (arg1 instanceof Array) {
+			propNames = arg1;
+		}
+
+		propNames.forEach(function(propName) {
+			var val = localStorage.getItem(propName);
+
+			if (val !== undefined && val !== null)
+				result[propName] = val;
+		});
+
+		callback(result);
+	};
+
+	Local.prototype.set = function(obj, callback) {
+		if (!this.isValid || !obj) return;
+
+		var propNames = Object.getOwnPropertyNames(obj);
+
+		propNames.forEach(function(propName) {
+			localStorage.setItem(propName, obj[propName]);
+		});
+
+		if (callback)
+			callback();
 	};
 
 	$(document).ready(function() {
